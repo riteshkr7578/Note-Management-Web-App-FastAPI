851ca4211fc753afdb1c0972a2e8e7e8a6ea9365
Ritesh <riteshkr7578@gmail.com>
2025-12-04 22:33:52 +0530

    first commit

---
+++ config/db.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+MONGO_URI="mongodb+srv://riteshkr75786278_db_user:mrqT8NvqW3aWZmcb@fastapi.empcn7v.mongodb.net/"
+
+conn=MongoClient(MONGO_URI)
---
+++ pythontypes.py
@@ -0,0 +1,8 @@
+#using for type hints
+
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))
